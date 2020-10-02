d0c524a1287ddb1b25479337e03456e5dbbaf20c
alastair <alastair@squaresetsound.com>
2020-10-02 13:55:13 +0100

    Formatting improvements to comply with flake8

--- seat/avrenderercontrol/osc_tascar_wsl.py
+++ seat/avrenderercontrol/osc_tascar_wsl.py
@@ -1,13 +1,19 @@
-# import av_renderer_control
-from .av_renderer_control import AVRendererControl
+# from .av_renderer_control import AVRendererControl
+import avrenderercontrol.av_renderer_control as avrc
 import confuse
 import pathlib
 import pandas as pd
 import pprint
+import ipaddress
+import os
+from pythonosc import udp_client
 
 # helper functions
 # leading underscore avoids being imported
+
+
 def _is_valid_ipaddress(addressToTest):
+    """Private function to check validity of an ip address"""
     try:
         parsedAddress = ipaddress.ip_address(addressToTest)
         print('parsedAddress:' + str(parsedAddress))
@@ -18,7 +24,7 @@ def _is_valid_ipaddress(addressToTest):
         return False
 
 
-class ListeningEffortPlayerAndTascarUsingOSCBase(AVRendererControl):
+class ListeningEffortPlayerAndTascarUsingOSCBase(avrc.AVRendererControl):
     """
     Base class to implement core functionality of co-ordinating the unity-based
     visual display with headtracking data sent via osc to tascar-based audio
@@ -26,7 +32,7 @@ class ListeningEffortPlayerAndTascarUsingOSCBase(AVRendererControl):
     """
     def __init__(self):
         # get the IP addresses
-        appName='ListeningEffortPlayerAndTascarUsingOSC'
+        appName = 'ListeningEffortPlayerAndTascarUsingOSC'
         self.moduleConfig = confuse.Configuration(appName, __name__)
 
     # implement conext manager magic
@@ -39,21 +45,23 @@ class ListeningEffortPlayerAndTascarUsingOSCBase(AVRendererControl):
 
     def setup_osc(self):
         # get the tascar ip address
-        # if specified in the config use it, otherwise look in enviornment variable
+        # if specified in the config use it, otherwise look in enviornment
+        # variable
         tascar_ipaddress = self.moduleConfig['tascar']['ipaddress'].get(str)
         print('config tascar.ipaddress:' + tascar_ipaddress)
         if not _is_valid_ipaddress(tascar_ipaddress):
-            variablename=self.moduleConfig['tascar']['ipenvvariable'].get(str)
-            filename=os.environ.get(variablename)
+            env_variable_name = self.moduleConfig['tascar']['ipenvvariable'] \
+                .get(str)
+            filename = os.environ.get(env_variable_name)
             with open(filename, "r") as myfile:
                 tascar_ipaddress = myfile.readline().strip()
-            print('env {}: {}'.format(variablename, tascar_ipaddress))
+            print('env {}: {}'.format(env_variable_name, tascar_ipaddress))
             if not _is_valid_ipaddress(tascar_ipaddress):
                 # failed to get a valid ipaddress
                 print(tascar_ipaddress)
                 raise ValueError
             # store it in config in case we need it again
-            self.moduleConfig['tascar']['ipaddress']=tascar_ipaddress
+            self.moduleConfig['tascar']['ipaddress'] = tascar_ipaddress
 
         # TODO: check validity of all ip addresses
         # open the OSC comms
@@ -74,9 +82,10 @@ class ListeningEffortPlayerAndTascarUsingOSCBase(AVRendererControl):
             self.moduleConfig['sampler']['source3']['oscport'].get(int))
 
         # tell unity where to send the head rotation data
-        self.video_client.send_message("/set_client_address",
-            [self.moduleConfig['tascar']['ipaddress'].get(str),
-             self.moduleConfig['tascar']['oscport'].get(int)])
+        self.video_client.send_message("/set_client_address", [
+            self.moduleConfig['tascar']['ipaddress'].get(str),
+            self.moduleConfig['tascar']['oscport'].get(int)
+            ])
 
     def close_osc(self):
         # this isn't really necessary but avoids warnings in unittest
@@ -100,39 +109,37 @@ class TargetToneInNoise(ListeningEffortPlayerAndTascarUsingOSCBase):
     # Override constructor to get the right config settings
     def __init__(self):
         # get the IP addresses
-        app_name='TargetToneInNoise'
+        app_name = 'TargetToneInNoise'
         self.moduleConfig = confuse.Configuration(app_name, __name__)
 
-    def load_config(self,config):
+    def load_config(self, config):
         # grab the bits we need
         # - blocks are presented in order
-        # current_block_index=config["state_control"]["current_block_index"].get(int)
-        # print('current_block_index: '+str(current_block_index))
-        # block_config = config["blocks"][current_block_index]
+        block_config = config["blocks"][
+            config["state_control"]["current_block_index"].get(int)
+            ]
+        pprint.pprint(block_config.get)
 
-        block_config = config["blocks"][config["state_control"]["current_block_index"].get(int)]
-        print('Block:')
-        print('\tid: '+block_config["id"].get(str))
-        print('\tlistening_condition_id: '+block_config["listening_condition_id"].get(str))
-
-
-        # - pandas dataframe makes it easy to find the right listening condition
+        # - pandas dataframe from list of dicts makes it easy to find the right
+        #   listening condition
         pprint.pprint(config["listening_conditions"].get())
-        df = pd.DataFrame(config["listening_conditions"].get()) # dataframe from list of dicts
-        pprint.pprint(df)
-        matching_index = df[df["id"] == block_config["listening_condition_id"].get(str)].index.values
+        df = pd.DataFrame(config["listening_conditions"].get())
+        matching_index = df[
+            df["id"] == block_config["listening_condition_id"].get(str)
+            ].index.values
         num_of_matches = len(matching_index)
-        # print('Looking for listening_conditions.id: ',block_config["listening_condition_id"].get(str))
-        # lc_df = df[ (df["id"] == block_config["listening_condition_id"].get(str))]
-        # num_of_matches = len(lc_df)
-        assert num_of_matches==1, "expected one match but got {}".format(num_of_matches)
+        assert num_of_matches == 1, \
+            "expected one match but got {}".format(num_of_matches)
         lc_config = config["listening_conditions"][matching_index[0]]
+        pprint.pprint(lc_config.get)
 
         # get the fully qualified path to the skybox video in stages
-        skybox_dir = self.skybox=pathlib.Path(config["paths"]["unity_data"]["root_dir"].get(str),
-            config["paths"]["unity_data"]["skybox_rel_dir"].get(str))
+        skybox_dir = self.skybox = pathlib.Path(
+            config["paths"]["unity_data"]["root_dir"].get(str),
+            config["paths"]["unity_data"]["skybox_rel_dir"].get(str)
+            )
         skybox_filename = lc_config["avrenderer"]["skybox_file"].get(str)
-        self.skybox_absolute_path = pathlib.Path(skybox_dir,skybox_filename)
+        self.skybox_absolute_path = pathlib.Path(skybox_dir, skybox_filename)
 
     def setup(self):
         pass
@@ -140,7 +147,7 @@ class TargetToneInNoise(ListeningEffortPlayerAndTascarUsingOSCBase):
     def start_scene(self):
         pass
 
-    def set_probe_level(self,probeLevel):
+    def set_probe_level(self, probeLevel):
         pass
 
     def present_next_trial(self):
