f749f005a1a57c6e05a7aa4f36b20942726c9bcd
alastair <alastair.h.moore@imperial.ac.uk>
2021-06-01 21:59:31 +0100

    Prompt user to use jacktripcontrol gui to start

--- seat/manual_tests/test_jacktrip_audio.py
+++ seat/manual_tests/test_jacktrip_audio.py
@@ -1,4 +1,6 @@
+import PySimpleGUI as sg
 import jacktripcontrol
+
 
 """
 Test that the metronome plays on the left channel
@@ -6,11 +8,18 @@ Test that the metronome plays on the left channel
 Assumes that there is no jack infrastructure setup yet
 """
 jtc = jacktripcontrol.JackTripControl()
-input('Press enter to start jtc')
+jtc.kill()
+print('In a moment, the JackTripControl gui will be presented. Check your settings,\n'
+      'press start, wait for status to show connected then close the gui.')
+input('Press Enter to proceed')
 
-#
-# jtc.start(connect_mode=jacktripcontrol.ConnectMode.NO_CONNECT)
-jtc.start(connect_mode=jacktripcontrol.ConnectMode.BLOCKING)
+gui = jacktripcontrol.Gui(jtc)
+gui.show()
+# input('Press enter to start jtc')
+
+# #
+# # jtc.start(connect_mode=jacktripcontrol.ConnectMode.NO_CONNECT)
+# jtc.start(connect_mode=jacktripcontrol.ConnectMode.BLOCKING)
 
 
 
@@ -19,8 +28,10 @@ jtc.start(connect_mode=jacktripcontrol.ConnectMode.BLOCKING)
 # print('This may take several seconds...')
 # input("Press Enter to continue with connecting JackTrip to the local soundcard...")
 # jtc.test_metronome_manual()
-
-input("Open QJackCtl and check its all ok. The press Enter to test with metronome")
+print('JackTripControl should now be running. Open QJackCtl and check its all ok. \n'
+      'press start, wait for status to show connected then close the gui.')
+input("When ready, press Enter to test audio with metronome")
+# sg.popup('Open QJackCtl and check its all ok. Then close this window to test with metronome')
 
 jtc.test_metronome_manual()
 
